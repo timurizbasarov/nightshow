fc086be4fb860a369b9498adc900be9c396898a3
timurizbasarov <timurizbasarov0703@gmail.com>
2024-05-29 21:19:55 +0600

    cat

---
+++ main.js
@@ -0,0 +1,11 @@
+
+$(function(){
+    $('.slider_inner').slick({
+        arrows:true,
+        dots:true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+      adaptiveHeight: true
+    });
+});
